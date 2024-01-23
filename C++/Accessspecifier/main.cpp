7dea70de3d46e99678ddf506459ec32b4633ce5b
lavanyam26 <lavanyamai01@gmail.com>
2024-01-23 13:37:18 +0530

    added files

--- C++/Accessspecifier/main.cpp
+++ C++/Accessspecifier/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 class accessspecifierdemo
@@ -25,5 +25,49 @@ int main()
     as.a=25;
     cout << as.a;
     as.printb();
+    return 0;
+}*/
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+class salaryprint
+{
+private:
+    int salary;
+public:
+    string empname;
+    void setsalary (int s)
+    {
+        salary=s;
+    }
+    int getsalary ()
+    {
+        return salary;
+    }
+    int getsalaryprint ()
+    {
+        cout << salary;
+    }
+    void getsalaryprint1 ()
+    {
+        cout << salary;
+    }
+};
+int main ()
+{
+    salaryprint sp;
+    sp.setsalary(50);
+    cout << sp.getsalary();
+    sp.getsalaryprint();
+    sp.getsalaryprint1();
+    if (50==50)
+    {
+        cout << "\nIts a return type";
+    }
     return 0;
 }
