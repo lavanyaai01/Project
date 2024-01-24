ee8c0d4063fca9faac8ee46f34b769e4002e5041
lavanyam26 <lavanyamai01@gmail.com>
2024-01-24 13:36:06 +0530

    added files

--- C++/Accessspecifier/main.cpp
+++ C++/Accessspecifier/main.cpp
@@ -33,7 +33,7 @@ int main()
 
 
 
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 class salaryprint
 {
@@ -70,4 +70,103 @@ int main ()
         cout << "\nIts a return type";
     }
     return 0;
+}*/
+
+
+
+
+
+#include<iostream>
+using namespace std;
+class Marklist
+{
+private:
+    int Roll_No;
+    string Name;
+    int English;
+    int Maths;
+    int Science;
+    int Total;
+public:
+
+    struct variables{
+    string Roll_No1;
+    string Name1;
+
+    };
+
+    void getdetails ()
+    {
+        cout << "Enter your Roll_No:";
+        cin >> Roll_No;
+        cout << "Enter your Name:";
+        cin >> Name;
+    }
+
+    string getdetails1 ()
+    {
+        variables va;
+        cout << "Enter your Roll_No:";
+        cin >> va.Roll_No1;
+        cout << "Enter your Name:";
+        cin >> va.Name1;
+        return va;
+    }
+    string displaydetails ()
+    {
+         variables va1;
+
+
+
+
+    }
+    variables vb;
+
+
+    /*
+
+    int getRollNo(){
+    return Roll_No;
+    }
+    int getName(){
+    return Name;
+    }
+    void getvalues ()
+    {
+        cout << "Enter your English Mark:";
+        cin >> English;
+        cout << "Enter your Maths Mark:";
+        cin >> Maths;
+        cout << "Enter your Science Mark:";
+        cin >> Science;
+    }
+    void setvalues ()
+    {
+        Total = (English+Maths+Science);
+    }
+    void getresult ()
+    {
+        displaydetails ();
+        cout << "\nTotal value is:" << Total;
+    }
+};
+
+*/
+
+};
+int main ()
+{
+    Marklist m;
+   /* m.getdetails ();
+    m.getvalues ();
+    m.setvalues ();
+    m.getresult ();
+    */
+
+    m.getdetails1 ();
+
+    cout << m.vb.Roll_No1;
+    cout << m.vb.Name1;
+
+    return 0 ;
 }
