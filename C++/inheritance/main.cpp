a65db463fb434adabd79c7eea857d4bee96bd2f7
lavanyam26 <lavanyamai01@gmail.com>
2024-01-24 13:40:27 +0530

    added files

---
+++ C++/inheritance/main.cpp
@@ -0,0 +1,42 @@
+/*#include <iostream>
+
+using namespace std;
+
+class cars
+{
+public:
+    string brand = "Nissan";
+    void MessagePrint ()
+    {
+        cout << "\nWelcome to Nissan";
+    }
+};
+class cartype
+{
+public:
+    string cartypename = "\nNissan SUV";
+    void MessagePrintfor ()
+    {
+        cout << "\nWelcome to Nissan SUV";
+    }
+};
+class NissanSUV : public cars , public cartype
+{
+
+};
+int main ()
+{
+    NissanSUV ns;
+    cout << ns.brand;
+    cout << ns.cartypename;
+    ns.MessagePrint ();
+    ns.MessagePrintfor();
+    return 0;
+}*/
+
+
+
+
+#include<iostream>
+using namespace std;
+class
