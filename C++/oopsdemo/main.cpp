c935ffb415fe35bce87ba9841a8b4e034b9b7db8
lavanyaai01 <lavanymai01@gmail.com>
2024-02-01 11:33:25 +0530

    added files

---
+++ C++/oopsdemo/main.cpp
@@ -0,0 +1,62 @@
+/*#include <iostream>
+
+using namespace std;
+
+namespace student
+{
+    string Name = "Lavanya";
+}
+namespace student1
+{
+    string Name1 = "\nLavanya";
+}
+using namespace student;
+using namespace student1;
+int main ()
+{
+    cout << Name;
+    cout << Name1;
+    return 0;
+}*/
+
+/*#include<iostream>
+using namespace std;
+int main ()
+{
+    string Name;
+    cout << "Enter your Name:";
+    cin >> Name;
+    cout << Name;
+    fflush (stdin);
+    cout << "\nEnter your Name:";
+    getline (cin,Name);
+    cout << Name ;
+    return 0;
+}*/
+
+
+/*#include<iostream>
+using namespace std;
+int main ()
+{
+    string firstname;
+    string lastname;
+    cout << "Enter your firstname:";
+    cin >> firstname;
+    cout << "Enter your lastname:";
+    cin >> lastname ;
+    cout << firstname + "  " + lastname << endl ;
+    string fullname = firstname.append (lastname);
+    cout << fullname << endl;
+    lastname.push_back ('l');
+    cout << lastname << endl;
+    firstname.push_back ('pp');
+    cout <<firstname << endl;
+    return 0;
+}*/
+
+fflush(stdin);
+cout << fullname.length() << endl;
+cout << fullname.size ()  << endl;
+return 0;
+}
