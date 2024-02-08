dceed79b55871e13761b0b730293273eb4e30c95
lavanyaai01 <lavanyamai01@gmail.com>
2024-02-08 13:42:24 +0530

    modified file

--- Python/Python_demo/functiondemo.py
+++ Python/Python_demo/functiondemo.py
@@ -1,4 +1,4 @@
-def sample():
+'''def sample():
     a=1+2+1
     print(a)
     if a==3:
@@ -9,9 +9,24 @@ def sample():
         x=x.split (" ")
         for x2 in x:
             print(x2)
-    elif a==4:
+    if a==4:
         print("a is 4")
+    if a==6:
+        print("a is 6")
+    else:
+        print("No value")
     y=input ("Enter the value:").split () 
     print (y)
-sample()
-    
+sample()'''
+i=0
+while i<10:
+    print(i)
+    if (i==9):
+        break
+    i+=1
+i=1
+while i<7:
+    i+=1
+    if (i==2):
+        continue 
+    print (i)
