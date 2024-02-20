17cf4920319d770393d0398eb4395fe2306fcbce
lavanyaai01 <lavanyamai01@gmail.com>
2024-02-20 13:30:41 +0530

    added files

--- Python/Python_demo/dbdemo/insertdemo.py
+++ Python/Python_demo/dbdemo/insertdemo.py
@@ -3,8 +3,16 @@ import mysql.connector
 win=Tk ()
 win.title("Insert into MySQL DB Demo")
 win.geometry("500x500")
-frameleft=Frame(win,bg="black",width=500,height=500,padx=30,pady=30)
-frameleft.pack(side=LEFT)
+class frameDBOperation:
+    def __init__(self):
+        frametop=Frame(win,bg="black",width=500,height=500,padx=30,pady=30)
+        frametop.pack(side=TOP)
+        btinsert=Button(frametop,text="INSERT",command=self.frameleft).pack(padx=10,pady=10)
+        btupdate=Button(frametop,text="UPDATE").pack(padx=10,pady=10)
+        btdelete=Button(frametop,text="DELETE").pack(padx=10,pady=10)
+    def frameleft(self):
+        frameleft=Frame(win,bg="black",width=500,height=500,padx=30,pady=30)
+        frameleft.pack(side=LEFT)
 frameright=Frame(win,bg="red",width=500,height=500)
 frameright.pack(side=RIGHT)
 lbl_Title_of_Operation=Label(frameleft,text="Insert into MySQL DB Demo")
