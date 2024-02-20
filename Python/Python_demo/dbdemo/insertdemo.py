e4716522f6379713e32a77de89140bca18a92b8f
lavanyaai01 <lavanyamai01@gmail.com>
2024-02-20 13:28:53 +0530

    added files

---
+++ Python/Python_demo/dbdemo/insertdemo.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import mysql.connector
+win=Tk ()
+win.title("Insert into MySQL DB Demo")
+win.geometry("500x500")
+frameleft=Frame(win,bg="black",width=500,height=500,padx=30,pady=30)
+frameleft.pack(side=LEFT)
+frameright=Frame(win,bg="red",width=500,height=500)
+frameright.pack(side=RIGHT)
+lbl_Title_of_Operation=Label(frameleft,text="Insert into MySQL DB Demo")
+lbl_Title_of_Operation.grid(row=0,columnspan=5)
+lblname=Label(frameleft,text="Name")
+lblname.grid(row=2,column=1,padx=30,pady=10)
+lblTamil=Label(frameleft,text="Tamil")
+lblTamil.grid(row=3,column=1,padx=30,pady=10)
+win.mainloop ()
